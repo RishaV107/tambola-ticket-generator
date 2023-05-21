2f0149a8c7983cff485906ac621d0ead8336b0b7
Rishav Singh <rishav.vorphy@gmail.com>
2023-05-21 23:21:40 +0530

    routes updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({ limit: "30mb", extend: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(ROUTE_CONSTS.ROUTES.USER.BASE_PATH, userRoutes);
+app.use("/api/users", userRoutes);
 app.use(ROUTE_CONSTS.ROUTES.TICKET.BASE_PATH, ticketRoutes);
 
 app.listen(config.port, () => {
--- constants.js
+++ constants.js
@@ -1,12 +1,12 @@
 export const ROUTE_CONSTS = {
   ROUTES: {
     USER: {
-      BASE_PATH: "api/users",
+      BASE_PATH: "/api/users",
       REGISTER_USER: "/register",
       LOGIN_USER: "/login",
     },
     TICKET: {
-      BASE_PATH: "api/tickets",
+      BASE_PATH: "/api/tickets",
       GET_TICKET: "/fetch",
       CREATE_TICKETS: "/create",
     },
